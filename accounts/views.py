fec9392d410f659fbc33a536b7b5e74a34da0f69
aditya98899 <adiverma9889@gmail.com>
2023-11-28 18:34:06 +0530

    first commit

---
+++ accounts/views.py
@@ -0,0 +1,52 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.contrib import messages
+
+def register_view(request):
+    #if form is submit 
+    if request.method == "POST":
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get(password)
+        cpassword = request.POST.get(cpass)
+        if password != cpassword or len(password) > 0 or len(cpassword) > 0:
+            messages.error( request,"passwords do not match")
+            return redirect('register')
+        #user create
+        #user create karo
+        user = user.objects.create_user (username, email, password)
+        messages.success (request, "Account created succesfully")
+        return redirect('login')
+    else:
+        return render(request, 'account/register.html')
+    
+def login_view(request):
+    if request.method == "POST":
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        if len(email) == 0 or len (password) == 0:
+            messages.error(request, "Invalid credentials")
+            return redirect ('login')
+        # user authenticate karo
+        User = authenticate (request, email=email, password=password)
+        if User is not None:
+            messages.success(request, "Loggout in successfully")
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, "Invalid credentials")
+            return redirect('login')
+    else:
+        return render(request, 'account/login.html')
+    
+def logout_view(request):
+    logout(request)
+    messages.success(request, "Logged out successfully")
+    return redirect( 'login')
+
+def profile_view(request):
+    return render(request, ' account/profile.html')
+
+
+
